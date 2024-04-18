5ff677be433bcb432626f5d7ed60c1c0b2f12799
jaganclt <jagan.clt37@gmail.com>
2024-04-18 11:54:09 +0530

    18-04_2024

---
+++ basics.py
@@ -0,0 +1,30 @@
+
+# a=45
+# salary=15000.50
+
+# # name='jagan'
+# print(a)
+# # print(name)
+
+# a=b=c=10
+
+# print(a)
+# #this is a comment
+# a,b,c=1,20.2,"hello world"
+# print(a,c)
+
+
+# a="jagan"
+# b=20.0
+# # print(a+b)
+# # print("-----------------------")
+# # a="hello"
+# # b="world"
+# # print(a+" "+b)
+
+# print(type(b))
+# print(len(a))
+
+a="jagan"
+b="renish"
+print(a+str(4)+b)
